ec0d17bccff389409fc5780002f8847af9b33f5a
Steven Kandow <kando1sr@gmail.com>
2020-02-28 16:39:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     outcome = programmer_hash[:dennis_ritchie][:languages]
+     outcome = programmer_hash[:dennis_ritchie][:languages][0]
      outcome
 end
 
